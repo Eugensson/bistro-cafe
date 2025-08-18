af6851ff530891b553695131d6347bc96204e784
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-08-18 21:04:32 +0300

    Connected to Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Lato, Playfair_Display } from "next/font/google";
 
 import "./globals.css";
@@ -35,6 +36,7 @@ export default function RootLayout({
         className={`${lato.variable} ${playfairDisplay.variable} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
