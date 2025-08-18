95dfdd86742a3547d517a9fe11e9c1bbea887e04
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2025-08-18 20:58:15 +0300

    Initialized project, added assets and styles, installed npm packages

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import type { Metadata } from "next";
+import { Lato, Playfair_Display } from "next/font/google";
+
+import "./globals.css";
+
+const lato = Lato({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+  variable: "--font-lato",
+});
+
+const playfairDisplay = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700", "800", "900"],
+  variable: "--font-playfair-display",
+});
+
+export const metadata: Metadata = {
+  title: {
+    default: "Bistro Cafe",
+    template: "%s - Bistro Cafe",
+  },
+  description:
+    "Bistro Cafe — a cozy spot for coffee lovers, fresh pastries, and warm gatherings. Discover the perfect blend of taste, comfort, and atmosphere in the heart of the city.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${lato.variable} ${playfairDisplay.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
